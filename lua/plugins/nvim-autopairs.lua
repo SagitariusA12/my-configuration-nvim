de86e31b5f9b96f9657fe12369935c7e66497bc9
SagitariusA12 <ruandavymtam@gmail.com>
2025-07-18 12:49:18 -0300

    Update

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,7 +1,45 @@
 return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"hrsh7th/nvim-cmp",
+	},
+	config = function()
+		local npairs = require("nvim-autopairs")
+		local Rule = require("nvim-autopairs.rule")
+
+		npairs.setup({
+			check_ts = true,
+			enable_check_bracket_line = false,
+			map_cr = true,
+			map_bs = true,
+			fast_wrap = {},
+		})
+
+		-- Integração com nvim-cmp
+		local cmp = require("cmp")
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+		-- Regras para mover sobre , ; :
+		for _, punct in pairs({ ",", ";", ":" }) do
+			npairs.add_rules({
+				Rule("", punct)
+					:with_pair(function()
+						return false
+					end)
+					:with_move(function(opts)
+						return opts.char == punct
+					end)
+					:with_del(function()
+						return false
+					end)
+					:with_cr(function()
+						return false
+					end)
+					:use_key(punct),
+			})
+		end
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,41 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua", "python", "javascript", "html", "css", "json", "bash", "markdown"
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-      auto_install = true,
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects", -- Adicione esta dependência
+	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+				"python",
+				"javascript",
+				"html",
+				"css",
+				"json",
+				"bash",
+				"markdown",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+			auto_install = true,
+			textobjects = {
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_end = {
+						["]d"] = { query = "@punctuation.special", desc = "Next punctuation" },
+					},
+				},
+			},
+		})
+		-- Mapeamento para pular delimitadores no modo insert
+		vim.keymap.set(
+			"i",
+			"<C-d>",
+			'<Cmd>lua require("nvim-treesitter.textobjects").move.goto_next_end("@punctuation.special")<CR><Space>',
+			{ noremap = true, silent = true }
+		)
+	end,
 }
-
